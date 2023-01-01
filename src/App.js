9ae851282fe8336aa41a14dd05b6a91dd848ac58
jeffondev <jeffondev@gmail.com>
2023-01-01 18:50:19 +0900

    로그인 화면 구성

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main-page'>
+          <div>
+            <h2>Plant a Dream</h2>
+           <input type="text" placeholder='아이디'></input>
+          </div>
+
+          <div>
+            <input type="text" placeholder='비밀번호'></input>
+          </div>
+          <button>로그인</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
