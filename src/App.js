5a4291bba47e3ee345d1e38e6d32d7e8f68e6e78
jeffondev <jeffondev@gmail.com>
2023-01-18 23:32:21 +0900

    login component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React, { useState } from 'react';
 import './App.css';
-import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Login from './components/Login';
@@ -9,47 +8,14 @@ import Login from './components/Login';
 function App() {
   let [Logined, setLogined] = useState(false);
 
-  let [이메일,이메일변경] = useState('');
-  let [비밀번호, 비밀번호변경] = useState('');
-  let [isErr, setIsErr] = useState(false);
-
-  function LoginClick(e) {
-    axios.post('/v1/login', {"email" : 이메일, "password" : 비밀번호})
-    .then((res)=> {
-      console.log(res); 
-      setLogined(true);
-    })
-    .catch((err) => {
-      console.error("[%d] msg: %s", err.response.status, err.response.data.msg);
-      setIsErr(true);
-    })
-  }
-
   if(!Logined) {
     return (
-      <div className="App">
-        <div>
-          <div className='main-page'>
-            <div>
-              <h2>Plant a Dream</h2>
-            <input type="text" placeholder='이메일' onChange={(e)=>{ 이메일변경(e.target.value) }}></input>
-            </div>
-
-            <div>
-              <input type="password" placeholder='비밀번호' onChange={(e)=>{ 비밀번호변경(e.target.value) }}></input>
-            </div>
-            <button onClick={() => {LoginClick()}}>로그인</button>
-            {isErr && (
-              <div className='login-err'>로그인 정보가 잘못되었습니다.</div>
-            )}
-          </div>
-        </div>
-      </div>
+      <Login />
     );
   }
   else {
     return (
-      <Login />
+      <div>안녕!! 난 메인</div>
     );
   }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,49 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Login() {
+  let [Logined, setLogined] = useState(false);
+
+  let [이메일,이메일변경] = useState('');
+  let [비밀번호, 비밀번호변경] = useState('');
+  let [isErr, setIsErr] = useState(false);
+
+  function LoginClick(e) {
+    console.log(이메일);
+    axios.post('/v1/login', {"email" : 이메일, "password" : 비밀번호})
+    .then((res)=> {
+      console.log(res); 
+      setLogined(true);
+    })
+    .catch((err) => {
+      console.error("[%d] msg: %s", err.response.status, err.response.data.msg);
+      setIsErr(true);
+    })
+  }
+
+  const handleEmailChange = (e) => {
+    이메일변경(e.target.value);
+  }
+
   return (
-    <div>안녕</div>
+    <div className="App">
+    <div>
+      <div className='main-page'>
+        <div>
+          <h2>Plant a Dream</h2>
+        <input type="text" placeholder='이메일' onChange={handleEmailChange}></input>
+        </div>
+
+        <div>
+          <input type="password" placeholder='비밀번호' onChange={(e)=>{ 비밀번호변경(e.target.value) }}></input>
+        </div>
+        <button onClick={() => {LoginClick()}}>로그인</button>
+        {isErr && (
+          <div className='login-err'>로그인 정보가 잘못되었습니다.</div>
+        )}
+      </div>
+    </div>
+  </div>
   );
 }
 
