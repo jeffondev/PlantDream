45e0624c89554eb4adffa9230bc8a4c56c8331f1
jeffondev <jeffondev@gmail.com>
2023-02-17 20:49:00 +0900

    checkbox 추가

--- src/components/SeedsToday/SeedCheck.js
+++ src/components/SeedsToday/SeedCheck.js
@@ -4,12 +4,15 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from "react-redux"
 import { pushSeed } from "../../store"
+import { useNavigate } from 'react-router-dom';
+import { MdDone, MdDelete } from 'react-icons/md';
 
 const CardSection = styled.div`
   width: 100%;
   height: 500px;
   background: skyblue;
   display : flex;
+  flex-direction: column;
   justify-content: center;
   align-items : center;
 `;
@@ -18,6 +21,26 @@ const CardTitle = styled.div`
   text-align: center;
   background-color: red;
   font-size: 60px;
+`;
+
+const CheckCircle = styled.div`
+  width: 96px;
+  height: 96px;
+  border-radius: 48px;
+  border: 6px solid #ced4da;
+  font-size: 88px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  ${props =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
 `;
 
 
@@ -31,6 +54,21 @@ function SeedCheck() {
       })
   }, [])
 
+  const navigate = useNavigate();
+  const handleCardClick = (id) => {
+    navigate(`/seeds/${id}`);
+  };
+
+  const handleCheckClick = (id) => {
+    // axios.post(`/v1/seeds/${id}/plant`)
+    // .then((res)=>{
+    //   axios.get(`/v1/seeds`)
+    //   .then((data)=>{
+    //     dispatch(pushSeed(data.data))
+    //   })
+    // })
+  }
+
   return (
     <Carousel>
       
@@ -38,7 +76,8 @@ function SeedCheck() {
       state.seeds.map((seed, index) => (
         <Carousel.Item key={index}>
             <CardSection>
-              <CardTitle>{seed.title}1</CardTitle>
+              <CheckCircle done={seed.done} onClick={handleCheckClick(seed.id)}>{!seed.done && <MdDone />}</CheckCircle>
+              <CardTitle onClick={() => handleCardClick(seed.id)}>{seed.title}</CardTitle>
             </CardSection>
           </Carousel.Item>
       ))
